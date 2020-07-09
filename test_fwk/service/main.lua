96e8cebbc2f27d9fe56cbc8af3108909b306f4ac
DESKTOP-3RTBBOP\31428 <314284230@qq.com>
2020-07-09 11:31:14 +0800

    修改正式的Main方法

--- test_fwk/service/main.lua
+++ test_fwk/service/main.lua
@@ -1,3 +1,10 @@
-local main = function()
-    Log("???????")
-end
+--引入或者说是创建一个skynet服务
+local skynet = require "skynet"
+
+
+--调用skynet.start接口，并定义传入回调函数
+skynet.start(
+    function()
+        skynet.error("Server First Test")
+    end
+)
