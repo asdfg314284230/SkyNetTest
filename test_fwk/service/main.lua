d52a423d0bab41be3f336a345f5fad3d0e38e231
DESKTOP-3RTBBOP\31428 <314284230@qq.com>
2020-07-11 15:52:15 +0800

    修改配置

--- test_fwk/service/main.lua
+++ test_fwk/service/main.lua
@@ -1,11 +1,23 @@
 --引入或者说是创建一个skynet服务
 local skynet = require "skynet"
 
-print("main函数执行")
-
 --调用skynet.start接口，并定义传入回调函数
 skynet.start(
     function()
-        skynet.error("Server First Test")
+        local loginserver = skynet.newservice("logind")
+        local gate = skynet.newservice("gated", loginserver)
+
+        skynet.call(
+            gate,
+            "lua",
+            "open",
+            {
+                port = 8888,
+                maxclient = 64,
+                servername = "sample"
+            }
+        )
+
+        
     end
 )
